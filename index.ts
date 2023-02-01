5999b59de8345d2602231e7d8dc145b142cd055a
Ingredi Silva <ingredisilva.dev@gmail.com>
2023-02-01 11:11:00 -0300

    feat(databases) database config

---
+++ index.ts
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+import { json } from "body-parser";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log(`Server Started at ${3000}`);
+});
+
+require("dotenv").config();
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error: any) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
